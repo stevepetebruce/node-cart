c4c331e9b10f4e7f3dc8d4811a8dc4b6d57e6623
stevepetebruce <stevepetebruce@hotmail.com>
2016-07-23 15:52:29 +0100

    handlebars loop through products

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product');
 
-/* GET home page. */
+/* Get home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping Cart' });
+  Product.find(function (err, docs) {
+    /* add 3 bootstrap columns in row and loop*/
+    var productBlocks = [];
+    var blockSize = 3;
+    for (var i = 0; i < docs.length; i += blockSize) {
+      productBlocks.push(docs.slice(i, i + blockSize));
+    }
+    res.render('shop/index', { title: 'Shopping Cart', products: productBlocks });
+  });
+
 });
 
 module.exports = router;
